d2e447c4081aaa61859950c632dbb5902f99a696
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-05-03 07:55:43 +0530

    Object-Assignment

--- app.js
+++ app.js
@@ -12,10 +12,33 @@
 
 
 // let itemsArray = [
-// {name:'juice', price:50, quantity:3},
-// {name:'cookie',price:30, quantity:9},
-// {name:'shirt',price:880, quantity:1},
-// {name:'pen',price:100, quantity:2}];
+//     {name:'juice', price:50, quantity:3},
+//     {name:'cookie',price:30, quantity:9},
+//     {name:'shirt',price:880, quantity:1},
+//     {name:'pen',price:100, quantity:2}
+// ];
+
+//                      :::::::    THIS IS BETTER WAY    :::::
+
+ 
+
+
+// let juicePrice =(itemsArray[0].price * itemsArray[0].quantity);
+// console.log(juicePrice);
+// let cookiePrice =(itemsArray[1].price * itemsArray[1].quantity);
+// console.log(cookiePrice);
+// let shirtPrice =(itemsArray[2].price * itemsArray[2].quantity);
+// console.log(shirtPrice);
+// let penPrice =(itemsArray[3].price * itemsArray[3].quantity);
+// console.log(penPrice);
+
+// let totalPrice = juicePrice + cookiePrice + shirtPrice + penPrice
+// console.log(totalPrice);
+
+
+
+//                      THIS IS NOT BETTER WAY 
+
 // let eachpricejuice =50*3
 // let eachpricecookie= 30*9
 // let eachpriceshirt =880*1
@@ -55,6 +78,29 @@
 // console.log(`This is the country ${ object.country}`);
 // console.log(`This is the first name ${ object.firstname}`);
 // console.log(`This is the last name ${ object.lastname}`);
+
+
+// Question No 3:
+
+// 3. Create a constructor function with some properties. Now 
+// create multiple records using the constructor.
+
+// SOLUTION :
+
+
+
+// let construct = [
+// {name : 'Waheed', position:'Supervisor',age :'39'},
+// {name : 'Hammad', position:'Supervisor',age :'40'},
+// {name : 'Sabir', position:'Head of supervision Depart',age :'45'},
+// {name : 'Jamshed', position:'Worker',age :'55'},
+
+// ];
+// console.log(construct[2].name);
+// console.log(construct[2].position);
+// console.log(construct[2].age);
+
+
 
 
 // Question No 4 :
